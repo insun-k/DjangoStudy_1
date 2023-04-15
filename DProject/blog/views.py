daef9994e0f390525f2b922e788c5304d56b3ba3
insun <kis8294@gmail.com>
2023-04-15 17:07:28 +0900

    글 상세 페이지 생성'

--- DProject/blog/views.py
+++ DProject/blog/views.py
@@ -14,3 +14,14 @@ def index(request):
             'posts':posts,
         }
     )
+
+
+def singlePost_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(
+        request,
+        'blog/singlePost_page.html',
+        {
+            'post':post,
+        }
+    )
