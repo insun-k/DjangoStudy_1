350c97934701244f9e7c5dab3766e0cc0cc40283
insun <kis8294@gmail.com>
2023-04-16 20:21:49 +0900

    글쓰기 폼, 글 수정하는 기능 만들기

--- DProject/blog/urls.py
+++ DProject/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('category/<str:slug>/', views.categories_page),
     path('category/no-category/',views.categories_page),
     path('tag/<str:slug>/', views.tag_page),
-
+    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view())
 ]
